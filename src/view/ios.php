955fb86b3ab02d4a4de7aea618733fb684187274
viper_work <wiperawa@gmail.com>
2020-07-21 15:46:39 +0500

    view fix

--- src/view/ios.php
+++ src/view/ios.php
@@ -14,24 +14,18 @@ use yii\helpers\Html;
 
 ?>
 
-<?=
-Html::tag('div',
-    Html::tag('div',
-        Html::tag('div',
-            <<<PATH
-<path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path>
-PATH.
-            Html::img($brandImg, ['class' => 'pwa-ios-brang-img']).
-            Html::tag('div',
-                $welcomeText.' '.$instructionText,
-                ['class' => 'pwa-ios-instruction-div']
-                )
-            ,
-            ['class' => 'pwa-ios-speech-bubble']
-            ),
-        ['class' => 'pwa-ios-install']
-    ),
-    array_merge(['class' => 'pwa-ios-install-container'], $containerOptions)
-    )
-?>
+<div <?= Html::renderTagAttributes(array_merge(['class' => 'pwa-ios-install-container'], $containerOptions)) ?>>
+    <div class="pwa-ios-install">
+        <div class="pwa-ios-speech-bubble">
+            <svg class="pwa-ios-close-install"
+                 viewBox="0 0 24 24">
+                <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path>
+            </svg>
 
+            <img class="pwa-ios-brand-img" src="<?= $brandImg ?>" alt="<?=Yii::$app->name?>"/>
+            <div class="pwa-ios-instruction-div">
+                <?= "{$welcomeText} {$instructionText}" ?>
+            </div>
+        </div>
+    </div>
+</div>
